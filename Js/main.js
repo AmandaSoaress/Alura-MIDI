efd51a55897435682e3c4a1139d64a82c7fcb1ea
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2023-04-21 12:17:15 -0300

    Implementando função dinâmica

--- Js/main.js
+++ Js/main.js
@@ -1,55 +1,13 @@
-//som tecla pom
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
-    console.log('tocaSomPom')
-}
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-//som tecla Clap
-function tocaSomClap() {
-    document.querySelector('#som_tecla_clap').play();
-    console.log('tocaSomClap')
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
-//som tecla Tim
-function tocaSomTim() {
-    document.querySelector('#som_tecla_tim').play();
-    console.log('tocaSomTim')
-}
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-//som tecla puff
-function tocaSomPuff() {
-    document.querySelector('#som_tecla_puff').play();
-    console.log('tocaSomPuff')
-}
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-//som tecla Splash
-function tocaSomSplash() {
-    document.querySelector('#som_tecla_splash').play();
-    console.log('tocaSomSlash')
-}
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-//som tecla Toim
-function tocaSomToim() {
-    document.querySelector('#som_tecla_toim').play();
-    console.log('tocaSomToim')
-}
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-//som tecla Psh
-function tocaSomPsh() {
-    document.querySelector('#som_tecla_psh').play();
-    console.log('tocaSomPsh')
-}
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-//som tecla Tic
-function tocaSomTic() {
-    document.querySelector('#som_tecla_tic').play();
-    console.log('tocaSomTic')
-}
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-//som tecla Tom
-function tocaSomTom() {
-    document.querySelector('#som_tecla_tom').play();
-    console.log('tocaSomTom')
-}
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
+const ListaDeAudio = document.querySelectorAll("audio");
 
+//som tecla dinâmica..............................................
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+for (let index = 0; index < ListaDeTeclas.length; index++) {
+    const botao = ListaDeTeclas[index];
+    botao.addEventListener('click', function (evento) {
+        const som = ListaDeAudio[index];
+        som.play();
+        console.log(som)
+    });
+}
